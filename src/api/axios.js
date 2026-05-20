eca4a38032c105ce850ac1f97b1b01d7da5a4086
Prabesh Shrestha <prabeshshrestha0112@gmail.com>
2026-05-20 16:58:16 +0545

    Ready for deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:3000"
-    : "http://192.168.18.136:3000";
+// const BASE_URL =
+//   window.location.hostname === "localhost"
+//     ? "http://localhost:3000"
+//     : "http://192.168.18.136:3000";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const axiosAuth = axios.create({
   baseURL: BASE_URL,
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,9 +1,11 @@
 import { io } from "socket.io-client";
 
-const SOCKET_ORIGIN =
-  window.location.hostname === "localhost"
-    ? "http://localhost:3000"
-    : "http://192.168.18.136:3000";
+// const SOCKET_ORIGIN =
+//   window.location.hostname === "localhost"
+//     ? "http://localhost:3000"
+//     : "http://192.168.18.136:3000";
+
+const SOCKET_ORIGIN = import.meta.env.VITE_API_URL;
 
 export const socket = io(SOCKET_ORIGIN, {
   autoConnect: false
